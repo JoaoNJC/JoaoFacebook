288d89986bca0a6e9013ed902777904991c75dbf
João Camba <joaonoeljose@gmail.com>
2023-06-16 00:23:33 +0100

    Image added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -6,6 +7,19 @@ const Header = () => {
       <h1>Header</h1>
 
       {/* Left  */}
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed" />
+        <div>
+          <SearchIcon className='h-6' />
+
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+
 
       {/* Center  */}
 
